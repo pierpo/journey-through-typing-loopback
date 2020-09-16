25463ca4cf2ba6bbfa26c098c715add80ce536bf
Pierre Poupin <pierrep@bam.tech>
2020-09-16 18:48:59 +0200

    [6] add solution

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,13 @@
 import { UserModel, VehicleModel } from './entities';
 import { QueryFilter } from './query';
 
+export type LoopbackInstance<TEntity> = TEntity & {
+  update: (payload: Partial<TEntity>) => void;
+};
+
 export interface LoopbackModel<TEntity> {
-  find(filter?: QueryFilter<TEntity>): Promise<TEntity[]>;
+  find(filter?: QueryFilter<TEntity>): Promise<LoopbackInstance<TEntity>[]>;
+  findOne(filter?: QueryFilter<TEntity>): Promise<LoopbackInstance<TEntity>>;
 }
 
 declare class Loopback {
