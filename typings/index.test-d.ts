d808dc7c811dd3ae94161032d823243d309345c3
Pierre Poupin <pierrep@bam.tech>
2020-09-16 17:06:51 +0200

    [2] add expected result (second model)

--- typings/index.test-d.ts
+++ typings/index.test-d.ts
@@ -1,12 +1,23 @@
 import { expectType } from 'tsd';
-import Loopback, { UserEntity, UserModel } from '.';
+import Loopback, {
+  UserEntity,
+  UserModel,
+  VehicleEntity,
+  VehicleModel,
+} from '.';
 
 const app = new Loopback();
 
 const tests = async () => {
+  // Users
   const users = await app.models.User.find();
   expectType<UserModel>(app.models.User);
   expectType<UserEntity[]>(users);
+
+  // Vehicles
+  const vehicles = await app.models.Vehicle.find();
+  expectType<VehicleModel>(app.models.Vehicle);
+  expectType<VehicleEntity[]>(vehicles);
 };
 
 export default tests;
