5ba0d7aa72d6130e1f151cc6e202a24c02989307
Pierre Poupin <pierrep@bam.tech>
2020-09-16 17:06:56 +0200

    [2] add naive solution

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,13 +3,22 @@ export interface UserEntity {
   age: number;
 }
 
+export interface VehicleEntity {
+  licencePlate: string;
+}
+
 export interface UserModel {
   find(): Promise<UserEntity[]>;
+}
+
+export interface VehicleModel {
+  find(): Promise<VehicleEntity[]>;
 }
 
 declare class Loopback {
   models: {
     User: UserModel;
+    Vehicle: VehicleModel;
   };
 }
 
