0daadefef4f9698f5967cebd3565c06a47b1bd2b
Pierre Poupin <pierrep@bam.tech>
2020-09-21 10:52:26 +0200

    [1] add expected result (Model.find)

--- typings/index.test-d.ts
+++ typings/index.test-d.ts
@@ -1,8 +1,12 @@
 import { expectType } from 'tsd';
-import Loopback from '.';
+import Loopback, { UserEntity, UserModel } from '.';
 
 const app = new Loopback();
 
-const tests = async () => {};
+const tests = async () => {
+  const users = await app.models.User.find();
+  expectType<UserModel>(app.models.User);
+  expectType<UserEntity[]>(users);
+};
 
 export default tests;
